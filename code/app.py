77a91130f31cd0db07244e87e47b69840eda6a85
Andreé Sandoval <andree.bit@gmail.com>
2021-01-31 17:02:27 -0500

    add CORS

--- code/app.py
+++ code/app.py
@@ -1,7 +1,10 @@
 from utils import recommender
 from models import book
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
